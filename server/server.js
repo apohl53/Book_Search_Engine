6d8fb8472b3223ee820af9856ed264baee629bce
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-11-15 19:36:28 -0500

    filled out resolvers.js  and typedefs.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const db = require("./config/connection");
+// const routes = require('./routes');
+const { typeDefs, resolvers } = require("./schema");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+// Apply Apollo Server as middleware
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+// Implement the Apollo Server and apply it to the Express server as middleware.
